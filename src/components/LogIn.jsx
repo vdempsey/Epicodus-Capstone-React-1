bb20e8872d514ee69e99abe45ca2f3394ae64a5f
Valia Dempsey <valentinaml@gmail.com>
2018-01-18 21:35:51 -0800

    Import BebasNeue font, override initial placeholder color in LogIn component

--- src/components/LogIn.jsx
+++ src/components/LogIn.jsx
@@ -3,6 +3,18 @@ import React from 'react';
 function LogIn(){
   return (
     <form>
+      <style jsx>{`
+        @import url('.././fonts/BebasNeue Bold.ttf');
+
+        button {
+          font-family: 'BebasNeue';
+        }
+
+        ::-webkit-input-placeholder {
+          color: #b5b5b5;
+        }
+
+      `}</style>
       <input type="text" placeholder="Enter your user name"></input>
       <input type="text" placeholder="Enter your password"></input>
       <button>LOGIN</button>
