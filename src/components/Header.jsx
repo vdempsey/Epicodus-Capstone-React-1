b0fa9f3ecd7b0936160ba3eeb09cdb3453a7952b
Valia Dempsey <valentinaml@gmail.com>
2018-01-18 22:42:15 -0800

    Import Open Sans font, add object literal for menu items

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import WebFont from 'webfontloader';
 import LogIn from './LogIn';
 import { Link } from 'react-router-dom';
 
+WebFont.load({
+  google: {
+    families: ['Open Sans :300,400,700', 'sans-serif']
+  }
+});
+
 function Header(){
+  let headerComponentStyles = {
+    backgroundColor: '#404040',
+    fontFamily: '"Open Sans", sans-serif',
+    paddingTop: '50px',
+    color: '#fff'
+  };
+  let linkColorStyles = {
+    color: '#fff',
+    textDecoration: 'none'
+  };
   return (
-    <div>
-      <div><Link to='/'>Home</Link> | <Link to='/AboutUs'>Blog</Link> | <Link to='/Contact'>Contact</Link></div>
+    <div style={headerComponentStyles}>
+      <div><Link style={linkColorStyles} to='/'>Home</Link> | <Link style={linkColorStyles} to='/AboutUs'>Blog</Link> | <Link style={linkColorStyles} to='/Contact'>Contact</Link></div>
       <div><Link to='/Register'><div><LogIn /></div></Link></div>
     </div>
   );
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import WebFont from 'webfontloader';
 import { AppContainer } from 'react-hot-loader';
 import { HashRouter } from 'react-router-dom';
+
+WebFont.load({
+  google: {
+    families: ['Open Sans :300,400,700', 'sans-serif']
+  }
+});
 
 const render = (Component) => {
   ReactDOM.render(
