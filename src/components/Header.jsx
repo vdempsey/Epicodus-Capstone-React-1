5f6836da7394dce86acc053c6f67265d1a3db0f8
Valia Dempsey <valentinaml@gmail.com>
2018-01-18 21:07:05 -0800

    Create LogIn component, add log in form to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import LogIn from './LogIn';
 import { Link } from 'react-router-dom';
 
 function Header(){
   return (
     <div>
-      <Link to='/'>Home</Link> | <Link to='/AboutUs'>Blog</Link> | <Link to='/Contact'>Contact</Link>
-
+      <div><Link to='/'>Home</Link> | <Link to='/AboutUs'>Blog</Link> | <Link to='/Contact'>Contact</Link></div>
+      <div><Link to='/Register'><div><LogIn /></div></Link></div>
     </div>
   );
 }
